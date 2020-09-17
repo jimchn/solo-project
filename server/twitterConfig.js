2a94e95c627033cf1d734cc0514f35b0bf89ca58
Jimmy Chen <36758617+jimchn@users.noreply.github.com>
2020-09-17 15:47:18 -0400

    Delete twitterConfig.js

--- server/twitterConfig.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    consumer_key: 'CSlRzKewGYSPriJ77COkYGUCX',
-    consumer_secret: 'h1ZtzBSSk11toUMF4Kic90TuZ34FEPYGW81b46IWB5G9T5vKh5',
-    access_token: '1237537370666393601-jNZDC9tfoSnFphNuSNWnxURQscdS7e',
-    access_token_secret: 'yBl7PYWGbxNelftmCgTTgMwpL2g6cKJMpSFmB8Bg4iitV'
-}
-
